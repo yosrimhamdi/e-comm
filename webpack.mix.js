8fbea8aef9bed057be77f7ef2cab9d9460b447c2
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-12 17:26:26 +0100

    Using browserSync for autoreload.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css', [
+    //
+  ]);
+
+mix.browserSync({
+  proxy: 'http://127.0.0.1:8000',
+  notify: false,
+});
+
+mix.disableNotifications();
