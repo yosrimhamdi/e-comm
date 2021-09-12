c9f5163b412ee69543a6fcb7729badb196a022b7
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-12 23:43:16 +0100

    Seeding users.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Factories\UserFactory;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run() {
+    // \App\Models\User::factory(10)->create();
+    $this->call(UserFactory::class);
+  }
 }
