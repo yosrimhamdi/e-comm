426bfd73e9626b719716a9bb380c09cfb28a3a3a
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-15 15:22:04 +0100

    Added login admin page.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
-{
-    //
+class AdminController extends Controller {
+  public function index() {
+    return view('admin.login');
+  }
+
+  public function store() {
+    echo 'will login';
+  }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -1,1 +1,8 @@
 <?php
+
+use App\Http\Controllers\AdminController;
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', [AdminController::class, 'index']);
+  Route::post('/login', [AdminController::class, 'store']);
+});
