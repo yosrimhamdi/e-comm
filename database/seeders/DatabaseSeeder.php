0f3d3bfd7ed63c77722474788d5405a8262c13a6
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-15 15:01:38 +0100

    Creating the admin resource.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder {
    * @return void
    */
   public function run() {
-    $this->call(UserSeeder::class);
+    $this->call([UserSeeder::class, AdminSeeder::class]);
   }
 }
