470042dada57b77ebbed19009853e8eb377e62f6
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-17 21:00:30 +0100

    Added Admin/Footer component.

--- resources/views/components/admin/base.blade.php
+++ resources/views/components/admin/base.blade.php
@@ -60,25 +60,7 @@
       </div>
     </div>
     <!-- /.content-wrapper -->
-    <footer class="main-footer">
-      <div class="pull-right d-none d-sm-inline-block">
-        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
-          <li class="nav-item">
-            <a
-              class="nav-link"
-              href="javascript:void(0)"
-            >FAQ</a>
-          </li>
-          <li class="nav-item">
-            <a
-              class="nav-link"
-              href="#"
-            >Purchase Now</a>
-          </li>
-        </ul>
-      </div>
-      &copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
-    </footer>
+    <x-admin.footer />
 
     <!-- Control Sidebar -->
     <aside class="control-sidebar">
