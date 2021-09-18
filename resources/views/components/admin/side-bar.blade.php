ade1688e8ee48ae768be85ad5b8e586d4da896d3
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-18 16:45:31 +0100

    [fix] 404 assets.

--- resources/views/components/admin/side-bar.blade.php
+++ resources/views/components/admin/side-bar.blade.php
@@ -8,7 +8,7 @@
           <!-- logo for regular state and mobile devices -->
           <div class="d-flex align-items-center justify-content-center">
             <img
-              src="../images/logo-dark.png"
+              src="{{ asset('admin/images/logo-dark.png') }}"
               alt=""
             >
             <h3><b>Sunny</b> Admin</h3>
