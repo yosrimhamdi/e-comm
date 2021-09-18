f7e051da142efc5e12f33d28d928e569e7d117dc
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-19 00:45:01 +0100

    Cleared console errors.

--- resources/views/components/admin/base.blade.php
+++ resources/views/components/admin/base.blade.php
@@ -66,12 +66,7 @@
   </div>
   <script src="{{ asset('/admin/js/vendors.min.js') }}"></script>
   <script src="{{ asset('/assets/icons/feather-icons/feather.min.js') }}"></script>
-  <script src="{{ asset('/assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>
-  <script src="{{ asset('/assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
-  <script src="{{ asset('/assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
-  <!-- Sunny Admin App -->
   <script src="{{ asset('/admin/js/template.js') }}"></script>
-  <script src="{{ asset('/admin/js/pages/dashboard.js') }}"></script>
 </body>
 
 </html>
