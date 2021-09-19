7a4a412ea437052c6252a463b144b23a7eb19292
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-19 22:19:43 +0100

    add views/home folde.

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('home.index');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])
+  ->get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard');
+  })
+  ->name('dashboard');
