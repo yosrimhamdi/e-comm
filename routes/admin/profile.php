f01d00484bb863599fa2448991457f98b0e0abef
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-16 13:16:48 +0100

    Changed code lines order.

--- routes/admin/profile.php
+++ routes/admin/profile.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\Admin\ProfileController;
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
   Route::get('/profile', [ProfileController::class, 'index']);
-  Route::get('/profile/edit', [ProfileController::class, 'editForm']);
   Route::post('/profile', [ProfileController::class, 'update']);
+  Route::get('/profile/edit', [ProfileController::class, 'editForm']);
 });
