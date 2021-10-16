bf460d1c33d4d15cf2e696fb7190fa25c47d502e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-16 13:22:26 +0100

    Regrouping all toastr dependencies into the toastr.blade.php file.

--- resources/views/components/admin/base.blade.php
+++ resources/views/components/admin/base.blade.php
@@ -36,16 +36,6 @@
     rel="stylesheet"
     href="{{ asset('/admin/css/skin_color.css') }}"
   >
-  <script
-    src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-    crossorigin="anonymous"
-  ></script>
-  <link
-    rel="stylesheet"
-    href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
-  >
-  <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 </head>
 
 <body class="hold-transition dark-skin sidebar-mini theme-primary fixed">
--- resources/views/components/toastr.blade.php
+++ resources/views/components/toastr.blade.php
@@ -1,4 +1,14 @@
 @if (session('toastr'))
+  <script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"
+  ></script>
+  <link
+    rel="stylesheet"
+    href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+  >
+  <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
   <script>
     const message = '{{ session('message') }}';
     const status = '{{ session('status') }}';
