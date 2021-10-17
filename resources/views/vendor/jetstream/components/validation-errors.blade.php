d20b8678ee9977080b0320874a7a50a37c8eb864
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-17 20:46:31 +0100

    Improved error messages styling.

--- resources/views/vendor/jetstream/components/validation-errors.blade.php
+++ resources/views/vendor/jetstream/components/validation-errors.blade.php
@@ -1,8 +1,6 @@
 @if ($errors->any())
   <div {{ $attributes }}>
-    <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
-
-    <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+    <ul style="background-color: #A94442; padding: .5em; margin-top: 1em; color: white">
       @foreach ($errors->all() as $error)
         <li>{{ $error }}</li>
       @endforeach
