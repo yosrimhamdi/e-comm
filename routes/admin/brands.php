905a81fb8c59c259b62c0b3b9e43dca88fed2bc9
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-23 00:07:29 +0100

    Removed a controller resource action.

--- routes/admin/brands.php
+++ routes/admin/brands.php
@@ -2,4 +2,4 @@
 
 use App\Http\Controllers\Admin\BrandController;
 
-Route::resource('/admin/brands', BrandController::class);
+Route::resource('/admin/brands', BrandController::class)->except('show');
